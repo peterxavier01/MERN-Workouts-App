54cb14f7e5ca1f66c98a0d1ad98c3a042641905c
Peter Uadiale <fxavy02@gmail.com>
2024-04-30 11:35:42 +0100

    feat: add logic to conditionally use server url depending on node env

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,3 +1,13 @@
 export const SET_WORKOUTS = "SET_WORKOUTS";
 export const CREATE_WORKOUT = "CREATE_WORKOUT";
 export const DELETE_WORKOUT = "DELETE_WORKOUT";
+
+let url;
+
+if (import.meta.env.DEV) {
+  url = "http://localhost:4000/api/workouts";
+} else if (import.meta.env.PROD) {
+  url = import.meta.env.PROD_URL;
+}
+
+export default url as string;
