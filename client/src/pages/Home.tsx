ea6cd63dc7e7fc669f99a1db2d70386aa423122d
Peter Uadiale <fxavy02@gmail.com>
2024-04-30 02:51:03 +0100

    feat: create the home route

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+
+import Navbar from "../components/Navbar";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
+import { IWorkout } from "../types";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { SET_WORKOUTS } from "../utils/constants";
+
+const Home = () => {
+  const { state, dispatch } = useWorkoutsContext();
+  const workouts = state.workouts;
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:4000/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        dispatch({ type: SET_WORKOUTS, payload: json });
+      }
+    };
+
+    fetchWorkouts();
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navbar />
+      <div className="pages">
+        <div className="home">
+          <div className="workouts">
+            {workouts &&
+              workouts.map((workout: IWorkout) => (
+                <>
+                  <WorkoutDetails key={workout._id} workout={workout} />
+                </>
+              ))}
+          </div>
+
+          <WorkoutForm />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
